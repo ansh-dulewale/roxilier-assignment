9c6f5f9d43bf85e5e1bf641ae64548bd8d13a9be
Ansh Dulewale <120741334+ansh-dulewale@users.noreply.github.com>
2025-09-10 19:35:06 +0530

    gdfdff

--- Assignment/backend/src/api/index.js
+++ Assignment/backend/src/api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 
 import emojis from "./emojis.js";
 import authRoutes from '../routes/auth.js';
+import storeRoutes from '../routes/store.js';
 
 const router = express.Router();
 
@@ -14,5 +15,6 @@ router.get("/", (req, res) => {
 
 router.use("/emojis", emojis);
 router.use("/auth", authRoutes);
+router.use("/store", storeRoutes);
 
 export default router;
